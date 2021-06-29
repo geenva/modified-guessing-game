a03c72b445acd6b910d5cbe683ff9a35bbec67c3
geenva <geenva@users.noreply.github.com>
2021-06-29 18:24:36 +0800

    🎉 Init

---
+++ src/main.rs
@@ -0,0 +1,48 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::env;
+use std::io;
+
+fn main() {
+    println!("Welcome to the Guessing Game.");
+    let mut args: Vec<String> = Vec::new();
+    for i in env::args().skip(1) {
+        args.push(i)
+    }
+
+    if args[0] == "dev" {
+        guessing_game(true);
+    } else {
+        guessing_game(false);
+    }
+
+}
+
+fn guessing_game(dev: bool) {
+    let num = rand::thread_rng().gen_range(1..10);
+
+    if dev == true {
+        println!("WARN: DEVELOPER MODE ON.");
+        println!("The number is {}.", &num)
+    }
+
+    loop {
+        let mut guess = String::new();
+        println!("Enter your guess... ");
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line.");
+
+        let guess: u32 = guess.trim().parse().expect("Please type a number.");
+
+        match guess.cmp(&num) {
+            Ordering::Less => println!("Too small! Your guess is {}.", &guess),
+            Ordering::Equal => {
+                println!("Equal! You win! Your guess is {}.", &guess);
+                break;
+            }
+            Ordering::Greater => println!("Too big! Your guess is {}.", &guess),
+        }
+    }
+}
